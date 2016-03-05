4aeaf6faececb8560a960ae4f456d141c21213a8
xavarr <xavier.arroues@aramisauto.com>
2016-03-05 05:09:21 +0100

    Optimization + Mise en param periode Crawl

--- src/Satisfaction/CrawlerBundle/Controller/DefaultController.php
+++ src/Satisfaction/CrawlerBundle/Controller/DefaultController.php
@@ -15,24 +15,7 @@ class DefaultController extends Controller
         curl_setopt($ch, CURLOPT_MAXREDIRS, 10 );
         curl_setopt($ch, CURLOPT_URL, ZDURL.$url);
         curl_setopt($ch, CURLOPT_USERPWD, ZDUSER."/token:".ZDAPIKEY);
-        switch($action){
-            case "POST":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-                break;
-            case "GET":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-                break;
-            case "PUT":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-                break;
-            case "DELETE":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-                break;
-            default:
-                break;
-        }
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
         curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -93,49 +76,52 @@ class DefaultController extends Controller
     }
     public function indexAction()
     {
+        $time_start = microtime(true);
         define("ZDAPIKEY", $this->getParameter('zendesk_api_key'));
         define("ZDUSER", $this->getParameter('zendesk_api_user'));
         define("ZDURL", $this->getParameter('zendesk_api_url'));
 
-        $today = date("Y-m-d");
-        $yesterday = date("Y-m-d", strtotime( '-1 days' ));
+        $nbJoursCrawl = $this->getParameter('zendesk_api_nombre_jours_crawler');
 
-        $page = 20;
+        $yesterday = date("Y-m-d", strtotime( '-'.(int)$nbJoursCrawl.' days' ));
+        $startTime = mktime(0, 0, 0, date('m'), date('d')-((int)$nbJoursCrawl+1), date('Y'));
+
         do {
-            $data = $this->curlWrap("/tickets.json?page=".$page, null, "GET");
+            $data = $this->curlWrap("/incremental/tickets.json?start_time=".$startTime, null, "GET");
             $tickets = $data->tickets;
             $count_result = count($data->tickets);
             for ($i=0;$i<$count_result;$i++){
-                $numticket = $tickets[$i]->id;
-                $description = $tickets[$i]->description;
-                $sujet = substr($tickets[$i]->description,0,50);
-                $requester_id = $tickets[$i]->requester_id;
-                $status = $tickets[$i]->status;
-                $updated_at = substr($tickets[$i]->updated_at,0,10);
-                if ($status == 'solved' || $updated_at >= $yesterday){
-                    $data_metrics = $this->curlWrap("/tickets/".$numticket."/metrics.json", null, "GET");
-                    $solved_at = substr($data_metrics->ticket_metric->solved_at,0,10);
+                if ($tickets[$i]->status == 'solved'){
+                    $data_metrics = $this->curlWrap("/tickets/".$tickets[$i]->id."/metrics.json", null, "GET");
                     $reopens_nb=$data_metrics->ticket_metric->reopens;
-
-                    if ($solved_at >= $yesterday) {
-                        $data_requester = $this->curlWrap("/users/" . $requester_id . ".json", null, "GET");
-                        $requester = $data_requester->user->email;
-                        $isThereATicket = $this->isThereATicket($numticket);
-                        $test = "nothing";
-                        if ($isThereATicket === $test) {
-                            echo "1e1kd1";
-                            $exec_ticket = $this->addTicket($numticket, $sujet, $description, $requester, $solved_at, $reopens_nb);
+                    if (substr($data_metrics->ticket_metric->solved_at,0,10) >= $yesterday) {
+                        $data_requester = $this->curlWrap("/users/" .  $tickets[$i]->requester_id . ".json", null, "GET");
+                        $isThereATicket = $this->isThereATicket($tickets[$i]->id);
+                        $nothingInBase = "nothing";
+                        if ($isThereATicket === $nothingInBase) {
+                            $this->addTicket(
+                                $tickets[$i]->id,
+                                substr($tickets[$i]->description,0,50),
+                                $tickets[$i]->description,
+                                $data_requester->user->email,
+                                substr($data_metrics->ticket_metric->solved_at,0,10),
+                                $reopens_nb
+                            );
                         }
                         else if ((int)$isThereATicket != (int)$reopens_nb) {
-                            echo "2eddk2";
-                            $exec_ticket = $this->updateTicket($numticket,$reopens_nb);
+                            $this->updateTicket(
+                                $tickets[$i]->id,
+                                $reopens_nb);
                         }
                     }
                 }
             }
-            $page = substr($data->next_page,56) ;
-            //  sleep(5);
-        } while ($count_result == 100); # $count_result == 100
+            $startTime = substr($data->next_page,74) ;
+        } while ($count_result == 1000); # $count_result == 1000
+
+        $time_end = microtime(true);
+
+        echo "Done in " . round($time_end - $time_start, 1) . " sec";
 
         return $this->render('SatisfactionCrawlerBundle:Default:index.html.twig');
     }
